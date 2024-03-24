f7a3ecbd88a3259eacb68db6b591326c3996c0a2
Vinícius <santsmarcos81@gmail.com>
2024-03-24 19:00:12 -0300

    Adicionando CSS

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,9 @@
 //import { Fragment } from "react"; //O mesmo que '<> </>'
+import "./styles.css";
 
 export function Details() {
   // Apenas um elemento pode ser permitido dentro do return
   return(
-    <>
       <h1>Hello World!</h1>
-      <span>Marcos Vinícius</span>
-    </>
   )
 }
