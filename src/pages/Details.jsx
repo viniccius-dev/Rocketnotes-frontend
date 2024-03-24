9b8008459289fa06502e38d0313d471687c1ae96
Vinícius <santsmarcos81@gmail.com>
2024-03-24 18:53:31 -0300

    Fragment

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,5 +1,11 @@
+//import { Fragment } from "react"; //O mesmo que '<> </>'
+
 export function Details() {
+  // Apenas um elemento pode ser permitido dentro do return
   return(
-    <h1>Hello World!</h1>  
+    <>
+      <h1>Hello World!</h1>
+      <span>Marcos Vinícius</span>
+    </>
   )
 }
