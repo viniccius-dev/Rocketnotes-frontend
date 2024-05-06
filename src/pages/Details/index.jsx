acf9440983a3fb864f76383cceec2d8bc98fd6cd
Vinícius <santsmarcos81@gmail.com>
2024-05-06 10:22:35 -0300

    Componente button

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,12 +1,19 @@
 //import { Fragment } from "react"; //O mesmo que '<> </>'
 import { Container } from './styles';
 
+import { Button } from '../../components/Button'
+
 export function Details() {
   // Apenas um elemento pode ser permitido dentro do return
   return(
     <Container>
       <h1>Hello World!</h1>
       <span>Marcos Vinícius</span>
+
+      <Button/>
+      <Button/>
+      <Button/>
+      <Button/>
     </Container>
   )
 }
