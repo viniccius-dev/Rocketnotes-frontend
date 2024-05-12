bbf0b487c3cdb64abde7b06231fcdb7215f23c33
Vinícius <santsmarcos81@gmail.com>
2024-05-11 21:36:56 -0300

    Finalizando o New

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -4,7 +4,8 @@ import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
 import { Textarea } from '../../components/Textarea'; 
 import { NoteItem } from '../../components/NoteItem'; 
-import { Section } from '../../components/Section'; 
+import { Section } from '../../components/Section';
+import { Button } from '../../components/Button';
 
 export function New() {
     return (
@@ -25,6 +26,15 @@ export function New() {
                         <NoteItem value="https://vinicciusdev.com" />
                         <NoteItem isNew placeholder="Novo link" />
                     </Section>
+
+                    <Section title="Marcadores">
+                        <div className="tags">
+                            <NoteItem value="react" />
+                            <NoteItem isNew placeholder="Nova tag" />
+                        </div>
+                    </Section>
+
+                    <Button title="Salvar" />
                 </Form>
             </main>
         </Container>
