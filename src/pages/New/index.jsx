3b6d9baf46ebb84b87c9be4df24eebb960933839
Vinícius <santsmarcos81@gmail.com>
2024-05-11 21:29:26 -0300

    Estilizando o NoteItem

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -3,6 +3,8 @@ import { Container, Form } from './styles';
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
 import { Textarea } from '../../components/Textarea'; 
+import { NoteItem } from '../../components/NoteItem'; 
+import { Section } from '../../components/Section'; 
 
 export function New() {
     return (
@@ -18,6 +20,11 @@ export function New() {
 
                     <Input placeholder="Título" />
                     <Textarea placeholder="Observações" />
+
+                    <Section title="Links úteis">
+                        <NoteItem value="https://vinicciusdev.com" />
+                        <NoteItem isNew placeholder="Novo link" />
+                    </Section>
                 </Form>
             </main>
         </Container>
