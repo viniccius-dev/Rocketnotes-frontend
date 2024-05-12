4517d9e97b77436cc089b4f89c93cd4119eecf51
Vinícius <santsmarcos81@gmail.com>
2024-05-11 22:16:22 -0300

    Navegacao

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,4 +1,5 @@
 import { Container, Form, Avatar } from './styles';
+import { Link } from 'react-router-dom';
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 
@@ -8,9 +9,9 @@ export function Profile() {
     return (
         <Container>
             <header>
-                <a href="/">
+                <Link to="/">
                     <FiArrowLeft />
-                </a>
+                </Link>
             </header>
 
             <Form>
