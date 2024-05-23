7fbdd928b860bec6f480aa899c34132825a5d742
Vinícius <santsmarcos81@gmail.com>
2024-05-23 10:40:32 -0300

    Corrigindo a instalacao do cors e axios para o front

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { FiMail, FiLock, FiUser } from 'react-icons/fi'
 import { Link } from 'react-router-dom';
 
@@ -7,6 +8,16 @@ import { Button } from '../../components/Button'
 import { Container, Form, Background } from './styles';
 
 export function SignUp() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleSignUp() {
+        if(!name || !email || !password) {
+            return alert("Preencha todos os campos!");
+        }
+    }
+
     return (
         <Container>
             <Background />
@@ -21,21 +32,24 @@ export function SignUp() {
                     placeholder="Nome"
                     type="text"
                     icon={FiUser}
+                    onChange={e => setName(e.target.value)}
                 />
 
                 <Input
                     placeholder="E-mail"
                     type="text"
                     icon={FiMail}
+                    onChange={e => setEmail(e.target.value)}
                 />
 
                 <Input
                     placeholder="Senha"
                     type="password"
                     icon={FiLock}
+                    onChange={e => setPassword(e.target.value)}
                 />
 
-                <Button title="Cadastrar" />
+                <Button title="Cadastrar" onClick={handleSignUp} />
 
                 <Link to="/">
                     Voltar para o login
