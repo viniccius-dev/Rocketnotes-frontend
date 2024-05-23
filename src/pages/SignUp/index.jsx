e3df62df3caf0f52478888f5a2ab1c1895f9260f
Vinícius <santsmarcos81@gmail.com>
2024-05-23 10:57:31 -0300

    Cadastro de usuarios

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { FiMail, FiLock, FiUser } from 'react-icons/fi'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+
+import { api } from '../../services/api';
 
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button'
@@ -12,10 +14,25 @@ export function SignUp() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+    const navigate = useNavigate();
+
     function handleSignUp() {
         if(!name || !email || !password) {
             return alert("Preencha todos os campos!");
         }
+
+        api.post("/users", { name, email, password })
+        .then(() => {
+            alert("Usuário cadastro com sucesso!");
+            navigate("/");
+        })
+        .catch(error => {
+            if(error.response){
+                alert(error.response.data.message);
+            } else {
+                alert("Não foi possível cadastrar");
+            }
+        })
     }
 
     return (
