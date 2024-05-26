6865f4a183bb84ebae6a0bafc430e1e99d3f8426
Vinícius <santsmarcos81@gmail.com>
2024-05-26 12:29:43 -0300

    Validando campos da nota

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -42,6 +42,18 @@ export function New() {
     }
 
     async function handleNewNote() {
+        if(!title) {
+            return alert("Digite o título da nota");
+        }
+
+        if(newLink) {
+            return alert("Você deixou um link no campo para adicionar, mas não clicou em inserir. Clique para adicionar ou deixe o campo vazio")
+        }
+
+        if(newTag) {
+            return alert("Você deixou uma tag no campo para adicionar, mas não clicou em inserir. Clique para adicionar ou deixe o campo vazio")
+        }
+
         await api.post("/notes", {
             title,
             description,
