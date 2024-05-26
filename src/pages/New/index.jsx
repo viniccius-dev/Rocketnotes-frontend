0c35321ec15ced652cda20faded6583ef5451d45
Vinícius <santsmarcos81@gmail.com>
2024-05-26 11:57:35 -0300

    Adicionar tag

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -12,6 +12,9 @@ import { Button } from '../../components/Button';
 export function New() {
     const [links, setLinks] = useState([]);
     const [newLink, setNewLink] = useState("");
+    
+    const [tags, setTags] = useState([]);
+    const [newTag, setNewTag] = useState("");
  
     function handleAddLink() {
         setLinks(prevState => [...prevState, newLink]);
@@ -20,6 +23,11 @@ export function New() {
 
     function handleRemoveLink(deleted) {
         setLinks(prevState => prevState.filter(link => link !== deleted));
+    }
+
+    function handleAddTag() {
+        setTags(prevState => [...prevState, newTag]);
+        setNewTag("");
     }
 
     return (
@@ -57,8 +65,22 @@ export function New() {
 
                     <Section title="Marcadores">
                         <div className="tags">
-                            <NoteItem value="react" />
-                            <NoteItem isNew placeholder="Nova tag" />
+                            {
+                                tags.map((tag, index) => (
+                                    <NoteItem
+                                        key={String(index)}
+                                        value={tag}
+                                        onClick={() => { }}   
+                                    />
+                                ))
+                            }
+                            <NoteItem 
+                                isNew 
+                                placeholder="Nova tag"
+                                onChange={e => setNewTag(e.target.value)}
+                                value={newTag}
+                                onClick={handleAddTag}
+                            />
                         </div>
                     </Section>
 
