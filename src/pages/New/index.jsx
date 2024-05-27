0ed020193cfb64ceed2315c23d498d0bbdbe5a6f
Vinícius <santsmarcos81@gmail.com>
2024-05-27 19:44:07 -0300

    Ajuste na navagacao

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,5 +1,5 @@
 import { Container, Form } from './styles';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 
 import { Header } from '../../components/Header';
@@ -8,6 +8,7 @@ import { Textarea } from '../../components/Textarea';
 import { NoteItem } from '../../components/NoteItem'; 
 import { Section } from '../../components/Section';
 import { Button } from '../../components/Button';
+import { ButtonText } from '../../components/ButtonText';
 
 import { api } from '../../services/api';
 
@@ -22,6 +23,10 @@ export function New() {
     const [newTag, setNewTag] = useState("");
 
     const navigate = useNavigate();
+
+    function handleBack() {
+      navigate(-1);
+    }
  
     function handleAddLink() {
         setLinks(prevState => [...prevState, newLink]);
@@ -62,7 +67,7 @@ export function New() {
         });
 
         alert("Nota criada com sucesso!");
-        navigate("/");
+        navigate(-1);
     }
 
     return (
@@ -73,7 +78,10 @@ export function New() {
                 <Form>
                     <header>
                         <h1>Criar nota</h1>
-                        <Link to="/">Voltar</Link>
+                        <ButtonText 
+                            title="Voltar" 
+                            onClick={handleBack} 
+                        />
                     </header>
 
                     <Input 
