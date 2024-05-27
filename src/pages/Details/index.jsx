c11266ff881ae1af5773faea05e98e61825516d1
Vinícius <santsmarcos81@gmail.com>
2024-05-27 19:30:47 -0300

    Remover nota

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -21,6 +21,15 @@ export function Details() {
     navigate("/");
   }
 
+  async function handleRemove() {
+    const confirm = window.confirm("Deseja realmente remover a nota?");
+
+    if(confirm) {
+      await api.delete(`/notes/${params.id}`);
+      navigate("/");
+    }
+  }
+
   useEffect(() => {
     async function fetchNote() {
       const response = await api.get(`/notes/${params.id}`);
@@ -38,7 +47,10 @@ export function Details() {
         <main>
           <Content>
 
-            <ButtonText title="Excluir nota"/>
+            <ButtonText 
+              title="Excluir nota"
+              onClick={handleRemove}
+            />
 
             <h1>
               {data.title}
